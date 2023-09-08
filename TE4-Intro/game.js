fda429588063702bc2b894ac0d34ba778051edb5
= <=>
2023-09-08 16:01:59 +0200

    FreEft (Funktionell men enormt bökig kod)

--- TE4-Intro/game.js
+++ TE4-Intro/game.js
@@ -2,55 +2,83 @@ export function game(element) {
     const start = element.querySelector('#start')
     const send = element.querySelector('#send')
 
-    let atributes = [0,0,0,0,0,0]
-    let position = 0
+    let canClick = true
+
+    let atributes = [0, 0, 0, 0, 0, 0]
+
+
+    let textarray = ["Felaktigt svar", "Vilken av följande attribut har Simon mest av?", "Vilken av följande attribut har Simon näst mest av?", ]
+    let textchoicearray = [ ,"1.Styrka, 2.Smidighet, 3.Tålighet, 4.Intelligens, 5.Visdom, 6.Karisma", "1.Styrka, 2.Smidighet, 3.Tålighet, 4.Intelligens, 5.Visdom, 6.Karisma"]
+    let choicearray = [ , 6,6,6,6,6,6,6,]
+    let gotoarray = [[3, 3, 3, 3, 3, 3], [3, 3, 3, 3, 3, 3]]
+    let firsttext = true
 
     start.addEventListener('click', (e) => {
 
         e.preventDefault
 
         const startbutton = document.getElementById("startbutton")
-        const answerfield = document.getElementById("answerfield")  
+        const answerfield = document.getElementById("answerfield")
         startbutton.style.display = "none"
-        answerfield.style.display = "block"  
+        answerfield.style.display = "block"
 
-        text("Vilken av följande attribut har Simon mest av?")
-        text("1.Styrka, 2.Smidighet, 3.Tålighet, 4.Intelligens, 5.Visdom, 6.Karisma")
+        event(1)
     })
 
-    send.addEventListener('click', (e) =>{
-
-        const answer = element.querySelector('#answer').value
-        e.preventDefault
-
-        if(answer == "1" || answer == "2" || answer == "3" || answer == "4" || answer == "5" || answer == "6"){
-
-            choice (answer,a,a)    
-
-        }
-        else{
-            text("Felaktigt svar")
-        }
-    })
-
-    function roll (atributes){
-        let result = Math.floor(Math.random()*20) +1 +atributes
-        return result        
+    function roll(atributes) {
+        let result = Math.floor(Math.random() * 20) + 1 + atributes
+        return result
     }
 
-    function text (String){
+    function text(id) {
         const storylist = element.querySelector('#storylist')
         const li = document.createElement('li')
 
-        li.textContent = String
+        
+        if(firsttext == true){
+            li.textContent = textarray[id]
+            firsttext = false
+            
+        }
+        else{
+            li.textContent = textchoicearray[id]
+            firsttext=true
+        }
         storylist.appendChild(li)
     }
 
-    function choice (answer,choiceCount,goto){
-        for(let i = 0; i < choiceCount; i++){
-            if (answer == i){
-                
-            }
+    function choice(answer, goto) {
+        canClick = true
+        if (answer) {
+
+        }
+        event(goto[answer - 1])
+    }
+
+    function event(id) {
+
+        console.log(id)
+        text(id)
+        text(id)
+        canClick = true
+
+        if (canClick = true) {
+            send.addEventListener('click', (e) => {
+
+                const answer = parseInt(element.querySelector('#answer').value)
+                e.preventDefault
+                canClick = false
+
+                if (answer <= choicearray[id] && answer >= 1) {
+                    choice(answer, [2, 2, 2, 2, 2, 2])
+                    console.log("Ett korrekt angivet svar")
+                } else {
+                    text(0)
+                    
+                    canClick = true
+                }
+                element.querySelector('#answer').value = ""
+            })
         }
     }
 }
